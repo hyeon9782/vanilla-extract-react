de7a127c7b6c6ea6367794a7d3ced096ef19a639
hyeon9782 <hyeon9782@gmail.com>
2023-12-21 02:17:54 +0900

    fix: useFunnel 타입 수정, react-router-dom 의존성 주입

--- src/hooks/useFunnel.tsx
+++ src/hooks/useFunnel.tsx
@@ -1,14 +1,13 @@
 import { ReactElement, ReactNode, useState } from "react";
 
 export function useFunnel(initStep: string) {
-  const [step, setStep] = useState(initStep);
+  const [step, setStep] = useState<string>(initStep);
 
   const Step = ({ children }: { children: ReactNode }) => {
     return <>{children}</>;
   };
 
-  const Funnel = ({ children }: { children: ReactElement[] }) => {
-    // name이 현재 step 상태와 동일한 Step만 렌더링
+  const Funnel: React.FC<{ children: ReactElement[] }> = ({ children }) => {
     const targetStep = children.find(
       (childStep) => childStep.props.name === step
     );
