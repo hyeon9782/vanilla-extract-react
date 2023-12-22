aa5bbad301a45a35e17375c2eb67d33bc8f05f41
hyeon9782 <hyeon9782@gmail.com>
2023-12-22 12:12:33 +0900

    ✨ feat: Select 컴포넌트 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import Select from "./components/Forms/Select/Select";
 import FunnelPage from "./pages/FunnelPage";
+
+const options = ["빨강", "파랑", "초록"];
 
 function App() {
   return (
     <div>
+      <Select placeholder="과일을 선택해주세요." options={options} />
       <FunnelPage />
     </div>
   );
--- src/components/Forms/Select/Select.tsx
+++ src/components/Forms/Select/Select.tsx
@@ -1,5 +1,32 @@
-const Select = () => {
-  return <div>Select</div>;
+import { useState } from "react";
+type SelectProps = {
+  placeholder: string;
+  size: string;
+  options: string[];
+};
+const Select = ({ placeholder, options, size }: SelectProps) => {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState("");
+
+  const toggleOpen = () => {
+    setOpen(!open);
+  };
+
+  const selectValue = (newValue: string) => {
+    setValue(newValue);
+  };
+  return (
+    <section>
+      <button onClick={toggleOpen}>{value ? value : placeholder}</button>
+      {open && (
+        <section>
+          {options.map((option) => (
+            <div onClick={() => selectValue(option)}>{option}</div>
+          ))}
+        </section>
+      )}
+    </section>
+  );
 };
 
 export default Select;
