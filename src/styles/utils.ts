42fb006a7b1888544babe0e97b6e16a8e522f49e
hyeon9782 <hyeon9782@gmail.com>
2023-12-25 03:24:33 +0900

    ✨ utils.ts 생성

---
+++ src/styles/utils.ts
@@ -0,0 +1,13 @@
+import { StyleRule } from "@vanilla-extract/css";
+import { Mode, tokens } from "../tokens";
+
+export const getVarName = (_value: string | null, path: string[]) =>
+  path.join("-").replace(".", "_").replace("/", "__");
+
+export const motionSafe = (style: StyleRule) => ({
+  "@media": {
+    "(prefers-reduced-motion: no-preference)": style,
+  },
+});
+
+export const getModeColors = (mode: Mode) => tokens.colors[mode];
